001bb3660562893895d1a2753dc24082e1581704
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2025-07-21 16:02:21 +0530

    chore: change create-graph.js to node.js

--- src/node.js
+++ src/node.js
@@ -1,16 +1,14 @@
 'use strict';
 
 class Node {
-  constructor(array) {
-    this.vertex = array;
+  constructor(vertex, creator = null) {
+    this.vertex = vertex;
 
-    // neighbors word is used for vertices describing possible positions on chessboard the knight could travel from given current position
-    this.neighbors = this.#findNeighbors(this.vertex);
+    // creator will be the neighbor node which created the current one.
+    this.creator = creator;
+    this.neighbors = this.findNeighbors(this.vertex);
   }
-
-  // private methods
-
-  #findNeighbors(vertex) {
+  findNeighbors(vertex) {
     let neighbors = [];
     const rowIndex = vertex[0];
     const columnIndex = vertex[vertex.length - 1];
@@ -18,66 +16,66 @@ class Node {
       switch (i) {
         case 0:
           {
-            const traverseColumn = columnIndex + 2;
-            const traverseRow = rowIndex - 1;
-            if (traverseColumn > 7 || traverseRow < 0) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            const traverseColumn = columnIndex - 1;
+            const traverseRow = rowIndex + 2;
+            if (traverseColumn < 0 || traverseRow > 7) break;
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 1:
           {
-            const traverseColumn = columnIndex + 1;
-            const traverseRow = rowIndex - 2;
-            if (traverseColumn > 7 || traverseRow < 0) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            const traverseColumn = columnIndex - 2;
+            const traverseRow = rowIndex + 1;
+            if (traverseColumn < 0 || traverseRow > 7) break;
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 2:
           {
-            const traverseColumn = columnIndex - 1;
-            const traverseRow = rowIndex - 2;
+            const traverseColumn = columnIndex - 2;
+            const traverseRow = rowIndex - 1;
             if (traverseColumn < 0 || traverseRow < 0) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 3:
           {
-            const traverseColumn = columnIndex - 2;
-            const traverseRow = rowIndex - 1;
+            const traverseColumn = columnIndex - 1;
+            const traverseRow = rowIndex - 2;
             if (traverseColumn < 0 || traverseRow < 0) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 4:
           {
-            const traverseColumn = columnIndex - 2;
-            const traverseRow = rowIndex + 1;
-            if (traverseColumn < 0 || traverseRow > 7) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            const traverseColumn = columnIndex + 1;
+            const traverseRow = rowIndex - 2;
+            if (traverseColumn > 7 || traverseRow < 0) break;
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 5:
           {
-            const traverseColumn = columnIndex - 1;
-            const traverseRow = rowIndex + 2;
-            if (traverseColumn < 0 || traverseRow > 7) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            const traverseColumn = columnIndex + 2;
+            const traverseRow = rowIndex - 1;
+            if (traverseColumn > 7 || traverseRow < 0) break;
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 6:
           {
-            const traverseColumn = columnIndex + 1;
-            const traverseRow = rowIndex + 2;
+            const traverseColumn = columnIndex + 2;
+            const traverseRow = rowIndex + 1;
             if (traverseColumn > 7 || traverseRow > 7) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
         case 7:
           {
-            const traverseColumn = columnIndex + 2;
-            const traverseRow = rowIndex + 1;
+            const traverseColumn = columnIndex + 1;
+            const traverseRow = rowIndex + 2;
             if (traverseColumn > 7 || traverseRow > 7) break;
-            neighbors.push([traverseColumn, traverseRow]);
+            neighbors.push([traverseRow, traverseColumn]);
           }
           break;
       }
